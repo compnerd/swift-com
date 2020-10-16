1975ae4142e8bdbc1cf9e0f271f5b9b5e0cf4170
Saleem Abdulrasool <compnerd@compnerd.org>
2020-10-15 21:34:46 -0700

    SwiftCOM: add a generic overload for `SHCreateItemFromParsingName`
    Use a generic type constraint to return a more refined type and infer
    the IID from the type.

--- Sources/SwiftCOM/Shell.swift
+++ Sources/SwiftCOM/Shell.swift
@@ -7,11 +7,22 @@
 
 import WinSDK
 
-public func SHCreateItemFromParsingName(_ pszPath: String, _ pbc: IBindCtx,
+public func SHCreateItemFromParsingName(_ pszPath: String, _ pbc: IBindCtx?,
                                         _ riid: inout IID) throws -> IUnknown {
   var pv: UnsafeMutableRawPointer?
   let hr: HRESULT =
-      SHCreateItemFromParsingName(pszPath.wide, pbc.raw, &riid, &pv)
+      SHCreateItemFromParsingName(pszPath.wide, pbc?.raw, &riid, &pv)
   guard hr == S_OK else { throw COMError(hr: hr) }
   return IUnknown(pUnk: pv)
+}
+
+public func SHCreateItemFromParsingName<T: IUnknown>(_ pszPath: String,
+                                                     _ pbc: IBindCtx?)
+    throws -> T {
+  var riid: IID = T.IID
+  var pv: UnsafeMutableRawPointer?
+  let hr: HRESULT =
+      SHCreateItemFromParsingName(pszPath.wide, pbc?.raw, &riid, &pv)
+  guard hr == S_OK else { throw COMError(hr: hr) }
+  return T(pUnk: pv)
 }
