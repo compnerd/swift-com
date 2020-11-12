787aff13a742bd74648e0832a9d1e4b6402644bc
Saleem Abdulrasool <compnerd@compnerd.org>
2020-11-11 19:53:51 -0800

    build: prefer building a shared library
    Generate a shared library for COM interfaces rather than a static
    library.

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 let SwiftCOM = Package(
   name: "SwiftCOM",
   products: [
-    .library(name: "SwiftCOM", targets: ["SwiftCOM"]),
+    .library(name: "SwiftCOM", type: .dynamic, targets: ["SwiftCOM"]),
   ],
   targets: [
     .target(name: "SwiftCOM", linkerSettings: [.linkedLibrary("Ole32")])
