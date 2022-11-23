c753a1653beb1395487a32576a1215cf4ea1b13a
Greg Cotten <greg@gregcotten.com>
2022-11-22 16:13:52 -0800

    fix for swift 5.7.1

--- Sources/SwiftCOM/Interfaces/Human/ID3D12DescriptorHeap.swift
+++ Sources/SwiftCOM/Interfaces/Human/ID3D12DescriptorHeap.swift
@@ -35,7 +35,7 @@ public class ID3D12DescriptorHeap: ID3D12Pageable {
   public func GetDesc() throws -> D3D12_DESCRIPTOR_HEAP_DESC {
     return try perform(as: WinSDK.ID3D12DescriptorHeap.self) { pThis in
       var desc: D3D12_DESCRIPTOR_HEAP_DESC = D3D12_DESCRIPTOR_HEAP_DESC()
-      let _ = pThis.pointee.lpVtbl.pointee.GetDesc(pThis, &desc)
+      desc = pThis.pointee.lpVtbl.pointee.GetDesc(pThis)
       return desc
     }
   }
--- Sources/SwiftCOM/Interfaces/Human/ID3D12Device.swift
+++ Sources/SwiftCOM/Interfaces/Human/ID3D12Device.swift
@@ -189,7 +189,7 @@ public class ID3D12Device: ID3D12Object {
   public func GetAdapterLuid() throws -> LUID {
     return try perform(as: WinSDK.ID3D12Device.self) { pThis in
       var luid: LUID = LUID()
-      let _ = pThis.pointee.lpVtbl.pointee.GetAdapterLuid(pThis, &luid)
+      luid = pThis.pointee.lpVtbl.pointee.GetAdapterLuid(pThis)
       return luid
     }
   }
@@ -208,7 +208,7 @@ public class ID3D12Device: ID3D12Object {
   public func GetCustomHeapProperties(_ nodeMask: UINT, _ heapType: D3D12_HEAP_TYPE) throws -> D3D12_HEAP_PROPERTIES {
     return try perform(as: WinSDK.ID3D12Device.self) { pThis in
       var properties: D3D12_HEAP_PROPERTIES = D3D12_HEAP_PROPERTIES()
-      let _ = pThis.pointee.lpVtbl.pointee.GetCustomHeapProperties(pThis, &properties, nodeMask, heapType)
+      properties = pThis.pointee.lpVtbl.pointee.GetCustomHeapProperties(pThis, nodeMask, heapType)
       return properties
     }
   }
@@ -234,7 +234,7 @@ public class ID3D12Device: ID3D12Object {
   public func GetResourceAllocationInfo(_ visibleMask: UINT, numResourceDescs: UINT, _ pResourceDescs: UnsafePointer<D3D12_RESOURCE_DESC>?) throws -> D3D12_RESOURCE_ALLOCATION_INFO {
     return try perform(as: WinSDK.ID3D12Device.self) { pThis in
       var info: D3D12_RESOURCE_ALLOCATION_INFO = D3D12_RESOURCE_ALLOCATION_INFO()
-      let _ = pThis.pointee.lpVtbl.pointee.GetResourceAllocationInfo(pThis, &info, visibleMask, numResourceDescs, pResourceDescs)
+      info = pThis.pointee.lpVtbl.pointee.GetResourceAllocationInfo(pThis, visibleMask, numResourceDescs, pResourceDescs)
       return info
     }
   }
